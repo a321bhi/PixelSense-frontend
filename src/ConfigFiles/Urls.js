0d392999561b49d55e45d4c4ecc3ebd384eabd88
a321bhi <a321bhi@gmail.com>
2022-08-05 11:30:46 +0530

    updated url

--- src/ConfigFiles/Urls.js
+++ src/ConfigFiles/Urls.js
@@ -1,11 +1,5 @@
-// let ip="35.208.17.20";
-let ip="localhost";
 
-// export const baseUrl ="http://"+ip+":8090";
-// export const feedUrl = "http://"+ip+":8102";
-// export const chatServiceUrl = "http://"+ip+":8103";
-
-
+let ip="ec2-3-111-217-124.ap-south-1.compute.amazonaws.com";
 
 export const userServiceUrl ="http://"+ip+":8081";
 export const storyServiceUrl = "http://"+ip+":8081";
