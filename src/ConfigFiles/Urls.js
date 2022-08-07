69fea8b8c1c8e887107e55097425a4d8996b9bb2
a321bhi <a321bhi@gmail.com>
2022-08-07 15:55:22 +0530

    Update Urls.js

--- src/ConfigFiles/Urls.js
+++ src/ConfigFiles/Urls.js
@@ -2,7 +2,7 @@
 // let ip="ec2-3-111-217-124.ap-south-1.compute.amazonaws.com";
 let ip="35.244.57.66"
 
-export const userServiceUrl ="https://"+ip+":8081";
-export const storyServiceUrl = "https://"+ip+":8081";
-export const mediaServiceUrl = "https://"+ip+":8081";
+export const userServiceUrl ="https://"+ip+":443";
+export const storyServiceUrl = "https://"+ip+":443";
+export const mediaServiceUrl = "https://"+ip+":443";
 export const chatServiceUrl = "http://"+ip+":8103";
