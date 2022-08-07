1d98f366d6848dd3dc8615dfa16dec188ee8c8ac
a321bhi <a321bhi@gmail.com>
2022-08-07 15:28:04 +0530

    updated to https

--- src/ConfigFiles/Urls.js
+++ src/ConfigFiles/Urls.js
@@ -2,7 +2,7 @@
 // let ip="ec2-3-111-217-124.ap-south-1.compute.amazonaws.com";
 let ip="35.244.57.66"
 
-export const userServiceUrl ="http://"+ip+":8081";
-export const storyServiceUrl = "http://"+ip+":8081";
-export const mediaServiceUrl = "http://"+ip+":8081";
+export const userServiceUrl ="https://"+ip+":8081";
+export const storyServiceUrl = "https://"+ip+":8081";
+export const mediaServiceUrl = "https://"+ip+":8081";
 export const chatServiceUrl = "http://"+ip+":8103";
