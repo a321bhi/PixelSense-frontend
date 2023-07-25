55af8b47e1b5a46aacee0e7d77ebf78f0c32b04b
Abhijit <a321bhi@gmail.com>
2023-07-25 07:57:13 +0530

    Update Urls.js

--- src/ConfigFiles/Urls.js
+++ src/ConfigFiles/Urls.js
@@ -1,8 +1,14 @@
 
 // let ip="ec2-3-111-217-124.ap-south-1.compute.amazonaws.com";
-let ip="35.244.57.66"
+let ip="localhost"
 
-export const userServiceUrl ="https://"+ip+":443";
-export const storyServiceUrl = "https://"+ip+":443";
-export const mediaServiceUrl = "https://"+ip+":443";
-export const chatServiceUrl = "http://"+ip+":8103";
+// export const userServiceUrl ="http://"+ip+":8090";
+// export const storyServiceUrl = "http://"+ip+":8102";
+// export const mediaServiceUrl = "http://"+ip+":8101";
+// export const chatServiceUrl = "http://"+ip+":8103";
+
+
+export const userServiceUrl ="http://"+ip+":8081";
+export const storyServiceUrl = "http://"+ip+":8081";
+export const mediaServiceUrl = "http://"+ip+":8081";
+export const chatServiceUrl = "http://"+ip+":8081";
